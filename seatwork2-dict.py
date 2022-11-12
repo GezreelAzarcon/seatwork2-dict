da0a0c6c6b9b120fa1a0efcfef84efeb99da7414
GezreelAzarcon <gezreelwazarcon@gmail.com>
2022-11-12 14:32:05 +0800

    Option 3 done, and designs added

--- seatwork2-dict.py
+++ seatwork2-dict.py
@@ -1,11 +1,11 @@
 # Create a menu with 3 options
-print("=================================")
-print("||            MENU             ||")
-print("---------------------------------")
-print("||       Add an item --> 1     ||")
-print("||   Search for an item --> 2  ||")
-print("||      Exit Program --> 3     ||")
-print("=================================")
+print("===================================================")
+print("||                     MENU                      ||")
+print("---------------------------------------------------")
+print("||               Add an item --> 1               ||")
+print("||           Search for an item --> 2            ||")
+print("||               Exit Program --> 3              ||")
+print("===================================================")
 
 # create an empty main dictionary (Created dictionaries will be saved here)
 alldict = {}
@@ -20,31 +20,39 @@ while True:
     if menu == 1:
 
         # Ask input for every personal data
-        username = input("What is your name? ")
-        age = int(input("What is your age? "))
-        address = input("What is your address? ")
-        phone_num = input("What is your phone number? ")
-        print("You have successfully added this user")
-
+        print(          "===================================================")
+        username = input( "||What is your name? ")
+        age = int(input(  "||What is your age? "))
+        address = input(  "||What is your address? ")
+        phone_num = input("||What is your phone number? ")
+        print(          "---------------------------------------------------")
+        print(          "||      You have successfully added this user    ||")
+        print(          "===================================================")
         # Append gathered input into the empty main dictionary
         alldict[username] = {
                             "Name": username,
                             "Age": age,   
                             "Address": address,
                             "Phone Number": phone_num
-                            }
-    print(alldict)     
+                            }   
     # If 2, search
     if menu == 2:
-        who = input("Who do you want to find? Enter First Name: ")
+        print(        "===================================================")
+        who = input(  "||Who do you want to find? Enter First Name: ")
+        print(        "===================================================")
+
         if who in alldict:
-            print("User Exists!")
-            print("Name:", alldict[who]['Name'])
-            print("Age:", alldict[who]['Age'])
-            print("Address", alldict[who]['Address'])
-            print("Phone Number", alldict[who]['Phone Number'])
+            print("||                 User Exists!                   ||")
+            print( "---------------------------------------------------")
+            print("||Name:", alldict[who]['Name'], "                 ||")
+            print("||Age:", alldict[who]['Age'], "                   ||")
+            print("||Address", alldict[who]['Address'], "            ||")
+            print("||Phone Number", alldict[who]['Phone Number'], "  ||")
+            print( "===================================================")
         else:
-            print("User doesnt exist!")
+            print( "===================================================")
+            print("||              User doesn't exist!               ||")
+            print( "===================================================")
         
     
         # Use input to check if the dict user wants exists
@@ -53,8 +61,13 @@ while True:
     # If 3, ask if user wants to exit
         # Create an input asking user if they want to quit
     if menu == 3:
-        quitq = input("Do you want to quit? [y/n] ")
+        print(         "===================================================")
+        quitq = input( "||Do you want to quit? [y/n]: ")
+        print(         "===================================================")
+
                 # If yes, use break
         if quitq == 'y':
             break
             # else, continue
+        else:
+            continue
