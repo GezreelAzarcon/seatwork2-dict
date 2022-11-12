2dfa5edcd8a7a39aa4aa7778da45ed592488f137
GezreelAzarcon <gezreelwazarcon@gmail.com>
2022-11-12 15:04:58 +0800

    Unnecessary lines and comments cleaned and removed, first name input added

--- seatwork2-dict.py
+++ seatwork2-dict.py
@@ -21,20 +21,25 @@ while True:
 
         # Ask input for every personal data
         print(          "===================================================")
-        username = input( "||What is your name? ")
+        username = input( "||What is your first name? ")
+        fullname = input( "||What is your full name? ")
         age = int(input(  "||What is your age? "))
         address = input(  "||What is your address? ")
         phone_num = input("||What is your phone number? ")
+        sick = input(     "||Are You sick? ")
         print(          "---------------------------------------------------")
         print(          "||      You have successfully added this user    ||")
         print(          "===================================================")
+        
         # Append gathered input into the empty main dictionary
         alldict[username] = {
-                            "Name": username,
+                            "Name": fullname,
                             "Age": age,   
                             "Address": address,
-                            "Phone Number": phone_num
+                            "Phone Number": phone_num,
+                            "Sick": sick
                             }   
+    
     # If 2, search
     if menu == 2:
         print(        "===================================================")
@@ -44,30 +49,28 @@ while True:
         if who in alldict:
             print("||                 User Exists!                   ||")
             print( "---------------------------------------------------")
-            print("||Name:", alldict[who]['Name'], "                 ||")
-            print("||Age:", alldict[who]['Age'], "                   ||")
-            print("||Address", alldict[who]['Address'], "            ||")
-            print("||Phone Number", alldict[who]['Phone Number'], "  ||")
+            print("||Name:", alldict[who]['Name'])
+            print("||Age:", alldict[who]['Age'])
+            print("||Address", alldict[who]['Address'])
+            print("||Phone Number", alldict[who]['Phone Number'])
+            print("||Sick", alldict[who]['Sick'])
             print( "===================================================")
         else:
             print( "===================================================")
             print("||              User doesn't exist!               ||")
             print( "===================================================")
         
-    
-        # Use input to check if the dict user wants exists
-        # If exists, print the dictionary using for loop
-        # If not Continue
     # If 3, ask if user wants to exit
-        # Create an input asking user if they want to quit
     if menu == 3:
+
+        # Create an input asking user if they want to quit
         print(         "===================================================")
         quitq = input( "||Do you want to quit? [y/n]: ")
         print(         "===================================================")
 
-                # If yes, use break
+        # If yes, use break
         if quitq == 'y':
             break
-            # else, continue
+        # else, continue
         else:
-            continue
+            continue    
